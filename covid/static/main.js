36b19961429bd3dc6f55a87faab5f2712913759b
11-sakshi <53787262+11-sakshi@users.noreply.github.com>
2021-04-28 16:01:39 +0430

    First COmmit

---
+++ covid/static/main.js
@@ -0,0 +1,34 @@
+const url = 'https://api.covid19api.com/summary'
+const lastUpdatedDate = document.getElementById('last-updated-date')
+const totalCasesCount = document.getElementById('total-cases')
+const newCasesCount = document.getElementById('new-cases')
+const totalDeathsCount = document.getElementById('total-deaths')
+const newDeathsCount = document.getElementById('new-deaths')
+const totalRecoveredCount = document.getElementById('total-recovered')
+const newRecoveredCount = document.getElementById('new-recovered')
+const countryWiseData = document.getElementById('country-wise-data')
+
+fetch(url)
+.then(response => response.json())
+.then(data => {
+    lastUpdatedDate.innerHTML = data.Date
+    totalCasesCount.innerHTML = data['Global']['TotalConfirmed']
+    newCasesCount.innerHTML = data['Global']['NewConfirmed']
+    totalDeathsCount.innerHTML = data['Global']['TotalDeaths']
+    newDeathsCount.innerHTML = data['Global']['NewDeaths']
+    totalRecoveredCount.innerHTML = data['Global']['TotalRecovered']
+    newRecoveredCount.innerHTML = data['Global']['NewRecovered']
+
+    for (let i=0; i<data['Countries'].length; i++) {
+        countryWiseData.innerHTML += "<tr class='orbitron'>"+
+        "<td>"+(i+1)+"</td>"+
+        "<td>"+data['Countries'][i]['Country']+"</td>"+
+        "<td>"+data['Countries'][i]['TotalConfirmed']+"</td>"+
+        "<td>"+data['Countries'][i]['NewConfirmed']+"</td>"+
+        "<td>"+data['Countries'][i]['TotalDeaths']+"</td>"+
+        "<td>"+data['Countries'][i]['NewDeaths']+"</td>"+
+        "<td>"+data['Countries'][i]['TotalRecovered']+"</td>"+
+        "<td>"+data['Countries'][i]['NewRecovered']+"</td>"
+        +"</tr>"      
+    }
+})
