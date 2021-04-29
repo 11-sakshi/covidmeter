b41337ed4dfa167db8dfc47f174eff8bab509b87
animesharma3 <animesharma3@gmail.com>
2021-04-29 21:25:38 +0530

    Display a country data

--- covid/static/main.js
+++ covid/static/main.js
@@ -8,6 +8,33 @@ const totalRecoveredCount = document.getElementById('total-recovered')
 const newRecoveredCount = document.getElementById('new-recovered')
 const countryWiseData = document.getElementById('country-wise-data')
 
+const resultTotalCasesCount = document.getElementById('result-total-cases')
+const resultNewCasesCount = document.getElementById('result-new-cases')
+const resultTotalDeathsCount = document.getElementById('result-total-deaths')
+const resultNewDeathsCount = document.getElementById('result-new-deaths')
+const resultTotalRecoveredCount = document.getElementById('result-total-recovered')
+const resultNewRecoveredCount = document.getElementById('result-new-recovered')
+const resultCountryWiseData = document.getElementById('result-country-wise-data')
+
+const countryInput = document.getElementById('country-input')
+const checkButton = document.getElementById('check')
+const result = document.getElementById('result')
+
+function displayCountryData(data, countryName) {
+    let countries = data['Countries']
+    result.classList.remove('d-none')
+    countries.forEach(country => {
+        if (country['Country'] == countryName) {
+            resultTotalCasesCount.innerHTML = commaSeperatedInteger(country['TotalConfirmed'])
+            resultNewCasesCount.innerHTML = commaSeperatedInteger(country['NewConfirmed'])
+            resultTotalDeathsCount.innerHTML = commaSeperatedInteger(country['TotalDeaths'])
+            resultNewDeathsCount.innerHTML = commaSeperatedInteger(country['NewDeaths'])
+            resultTotalRecoveredCount.innerHTML = commaSeperatedInteger(country['TotalRecovered'])
+            resultNewRecoveredCount.innerHTML = commaSeperatedInteger(country['NewRecovered'])
+        }
+    })
+}
+
 function commaSeperatedInteger(num) {
     return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 }
@@ -22,6 +49,8 @@ fetch(url)
     newDeathsCount.innerHTML = commaSeperatedInteger(data['Global']['NewDeaths'])
     totalRecoveredCount.innerHTML = commaSeperatedInteger(data['Global']['TotalRecovered'])
     newRecoveredCount.innerHTML = commaSeperatedInteger(data['Global']['NewRecovered'])
+
+    checkButton.addEventListener('click', () => { displayCountryData(data, countryInput.value) })
 
 //     for (let i=0; i<data['Countries'].length; i++) {
 //         countryWiseData.innerHTML += "<tr class='orbitron'>"+
