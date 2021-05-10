4552e5801905f811791a3f7c4b2059171cd65cff
animesharma3 <animesharma3@gmail.com>
2021-05-10 21:41:12 +0530

    Displaying India's Time Series Data using Chart.js

--- covid/static/main.js
+++ covid/static/main.js
@@ -20,12 +20,68 @@ const countryInput = document.getElementById('country-input')
 const checkButton = document.getElementById('check')
 const result = document.getElementById('result')
 
+let myChart = document.getElementById('myChart')
+
 checkButton.addEventListener('click', () => {
-    console.log('in')
+    myChart.parentElement.classList.remove('d-none')
+    myChart.classList.remove('d-none')
+    getCasesTimeSeriesData()
     fetch(url)
     .then(response => response.json())
     .then(data => displayCountryData(data, countryInput.value))
 })
+
+function getCasesTimeSeriesData() {
+    let url = 'https://api.covid19india.org/data.json'
+    let date = []
+    let dailyConfirmed = []
+    let dailyDeceased = []
+    let dailyRecovered = []
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        let cases_time_series = data['cases_time_series']
+        cases_time_series.forEach(cases => {
+            dailyConfirmed.push(cases['dailyconfirmed'])
+            dailyDeceased.push(cases['dailydeceased'])
+            dailyRecovered.push(cases['dailyrecovered'])
+            date.push(cases['date'])
+        })
+        new Chart(myChart.getContext('2d'), {
+            type: 'line',
+            data: {
+                labels: date,
+                datasets: [{ 
+                    data: dailyConfirmed,
+                    label: "Daily Confirmed",
+                    borderColor: "blue",
+                    fill: false
+                },
+                { 
+                    data: dailyDeceased,
+                    label: "Daily Deceased",
+                    borderColor: "red",
+                    fill: false
+                },
+                { 
+                    data: dailyRecovered,
+                    label: "Daily Recovered",
+                    borderColor: "green",
+                    fill: false
+                }
+                ]
+            },
+            options: {
+                responsive: true,
+                title: {
+                display: true,
+                text: 'World population per region (in millions)'
+                }
+            }
+    });
+    })
+}
 
 function displayCountryData(data, countryName) {
     console.log('in')
